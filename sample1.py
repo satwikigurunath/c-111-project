1012bab7609df38f7b827c3da11d22fab94a0be9
satwikigurunath <76837776+satwikigurunath@users.noreply.github.com>
2022-03-20 16:34:13 +0530

    Add files via upload

---
+++ sample1.py
@@ -0,0 +1,73 @@
+import plotly.figure_factory as ff
+import plotly.graph_objects as go
+import statistics
+import random
+import pandas as pd
+import csv
+
+df = pd.read_csv("sample_1.csv")
+data = df["reading_time"].tolist()
+
+
+#plotting the graph
+# fig = ff.create_distplot([data],["Math Scores"], show_hist= False)
+# fig.show()
+
+#calculating the mean and standard deviation of the population data
+mean = statistics.mean(data)
+std_deviation = statistics.stdev(data)
+# print("mean of popultion:- ",mean)
+# print("Standard deviation of popultion:- ",std_deviation)
+
+
+
+##  code to find the mean of 100 data points 1000 times 
+#function to get the mean of the given data samples
+# pass the number of data points you want  as counter
+def random_set_of_mean(counter):
+    dataset = []
+    for i in range(0, counter):
+        random_index= random.randint(0,len(data)-1)
+        value = data[random_index]
+        dataset.append(value)
+    mean = statistics.mean(dataset)
+    return mean
+
+
+
+# Pass the number of time you want the mean of the data points as a parameter in range function in for loop
+mean_list = []
+for i in range(0,1000):
+    set_of_means= random_set_of_mean(100)
+    mean_list.append(set_of_means)
+
+
+## calculating mean and standard_deviation of the sampling distribution.
+std_deviation = statistics.stdev(mean_list)
+
+
+## findig the standard deviation starting and ending values
+first_std_deviation_start = mean-std_deviation
+first_std_deviation_end = mean+std_deviation
+second_std_deviation_start = mean-(2*std_deviation)
+second_std_deviation_end = mean+(2*std_deviation)
+third_std_deviation_start =  mean-(3*std_deviation)
+third_std_deviation_end =  mean+(3*std_deviation)
+
+
+# finding the mean of the THIRD data (STUDENTS WHO GOT FUNSHEET) and plotting it on the plot.
+df = pd.read_csv("Sample_1.csv")
+data = df["reading_time"].tolist()
+mean_of_sample1 = statistics.mean(data)
+print("mean of School 1 sample:- ",mean_of_sample1)
+fig = ff.create_distplot([mean_list], ["reading_time"], show_hist=False)
+fig.add_trace(go.Scatter(x=[mean, mean], y=[0, 0.17], mode="lines", name="MEAN"))
+fig.add_trace(go.Scatter(x=[mean_of_sample1, mean_of_sample1], y=[0, 0.17], mode="lines", name="MEAN OF STUDNETS WHO GOT FUNSHEETS"))
+fig.add_trace(go.Scatter(x=[second_std_deviation_end, second_std_deviation_end], y=[0, 0.17], mode="lines", name="STANDARD DEVIATION 2 END"))
+fig.add_trace(go.Scatter(x=[third_std_deviation_end, third_std_deviation_end], y=[0, 0.17], mode="lines", name="STANDARD DEVIATION 3 END"))
+fig.show()
+
+
+#finding the z score using the formula
+z_score = (mean - mean_of_sample1)/std_deviation
+print("The z score is = ",z_score)
